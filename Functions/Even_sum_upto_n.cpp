21eaf5e51101cec2f6b9263d82f48efb5d3bb83d
harsh242024 <harshgajraj24@gmail.com>
2025-08-24 10:27:17 +0530

    Update Even_sum_upto_n.cpp

--- Functions/Even_sum_upto_n.cpp
+++ Functions/Even_sum_upto_n.cpp
@@ -15,5 +15,5 @@ int main(){
     cin>> n;
     int sum = evenSum(n);
     cout << "Sum of Even numbers upto N numbers is : "<<sum;
-    return 0; // will return the value
+    return 0; // will return value
 }
